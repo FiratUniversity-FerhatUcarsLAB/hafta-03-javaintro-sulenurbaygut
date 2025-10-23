5c4472c8e9f0a7aba50aa6898f17c573b397394e
sulenurbaygut <250541029@firat.edu.tr>
2025-10-23 23:11:56 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya hoş geldiniz");
+        System.out.println("Bilgisayar bilimine hoş geldiniz");
+        System.out.println("Programlama eğlencelidir");
+    }
+}
