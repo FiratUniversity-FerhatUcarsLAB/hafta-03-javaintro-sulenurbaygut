18849b6a67eb7d0fd0ed7d8bf2c071c5710674d5
sulenurbaygut <250541029@firat.edu.tr>
2025-10-23 22:24:09 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,8 +1,9 @@
 class _BesKereTekrar {
     static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {} // parametreyi kullandık, uyarı gider
+        for (int i = 0; i < args.length; i++) {} //
         for (int i = 0; i < 5; i++) {
             System.out.println("Welcome to Java ");
         }
     }
 }
+
