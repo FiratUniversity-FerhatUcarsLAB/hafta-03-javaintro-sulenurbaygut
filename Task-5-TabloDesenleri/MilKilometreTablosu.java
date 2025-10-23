ee4db15256c746b9ce230db626cb286d21540fc4
sulenurbaygut <250541029@firat.edu.tr>
2025-10-23 23:41:13 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,12 @@
+public class DonusumTablosu {
+    public static void main(String[] args) {
+        System.out.println("Dönüşüm Tablosu:");
+        System.out.println("Mil → Kilometre (1 mil = 1.609 km)");
+        
+        System.out.println("1 mil = " + (1 * 1.609) + " km");
+        System.out.println("5 mil = " + (5 * 1.609) + " km");
+        System.out.println("10 mil = " + (10 * 1.609) + " km");
+        System.out.println("20 mil = " + (20 * 1.609) + " km");
+        System.out.println("50 mil = " + (50 * 1.609) + " km");
+    }
+}
