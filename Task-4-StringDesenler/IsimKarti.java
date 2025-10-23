746d7b6b96f52f16cbf42eaa510f91da37600761
sulenurbaygut <250541029@firat.edu.tr>
2025-10-23 23:32:31 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Şule Nur Bayğut";
+        String ogrenciNo = "250541029";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("******************************");
+        System.out.println("*        ÖĞRENCİ KARTI       *");
+        System.out.println("******************************");
+        System.out.println("* Ad          : " + ad);
+        System.out.println("* Öğrenci No  : " + ogrenciNo);
+        System.out.println("* Bölüm       : " + bolum);
+        System.out.println("******************************");
+    }
+}
